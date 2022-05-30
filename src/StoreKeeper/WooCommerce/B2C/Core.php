1d7cd41d8238b5df385d47f69c79fb2162e4b6ff
Junmar Jose <jnmrclmbsjse@gmail.com>
2022-05-30 22:00:55 +0800

    Harden error handling

--- src/StoreKeeper/WooCommerce/B2C/Core.php
+++ src/StoreKeeper/WooCommerce/B2C/Core.php
@@ -413,10 +413,8 @@ HTML;
         $dirs = self::getPossibleTmpDirs();
 
         foreach ($dirs as $dir) {
-            if (!file_exists($dir)) {
-                if (false === mkdir($dir, '0777', true)) {
-                    continue; // failed to create
-                }
+            if (!file_exists($dir) && !mkdir($dir, '0777', true) && !is_dir($dir)) {
+                continue; // failed to create
             }
             if (is_writable($dir)) {
                 return $dir;
