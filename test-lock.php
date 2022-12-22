77c0f467526f08829d48a32e8021282a042eb1e9
luki <szymon@storekeeper.nl>
2022-12-22 17:03:51 +0100

    added sleep to lock test

--- test-lock.php
+++ test-lock.php
@@ -4,4 +4,6 @@ include_once __DIR__.'/autoload.php';
 
 $lock = new \StoreKeeper\WooCommerce\B2C\Tasks\BackrefImportLock('aaa');
 $lock->lock();
-echo 'Locked';
+echo 'Locked (sleep 60s)';
+sleep(60);
+echo 'released lock';
