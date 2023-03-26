8fe6ddc7c2ffe2f0828daf203f9dc81b8b5bc3b0
luki <szymon@storekeeper.nl>
2023-03-26 17:08:32 +0200

    Added more meta rendering

--- src/StoreKeeper/WooCommerce/B2C/Frontend/Handlers/StoreKeeperSeoHandler.php
+++ src/StoreKeeper/WooCommerce/B2C/Frontend/Handlers/StoreKeeperSeoHandler.php
@@ -58,11 +58,32 @@ class StoreKeeperSeoHandler implements WithHooksInterface
             $seo = StoreKeeperSeo::getProductSeo($product);
             $this->renderHeadMetaSeo($seo);
             $this->renderProductSocialMeta($seo, $product);
+        } elseif (is_singular()) {
+            $post = get_queried_object();
+            if ($post instanceof \WP_Post) {
+                $this->renderPostSocialMeta($post);
+            } else {
+                $this->renderSocialMetaFallback();
+            }
+            $this->renderMetaDescription();
         } elseif (is_product_category()) {
             $term = get_queried_object();
             if ($term instanceof \WP_Term) {
                 $seo = StoreKeeperSeo::getCategorySeo($term);
                 $this->renderHeadMetaSeo($seo);
+                $this->renderTermSocialMeta($term);
+            } else {
+                $this->renderSocialMetaFallback();
+                $this->renderMetaDescription();
+            }
+        } else {
+            $term = get_queried_object();
+            if ($term instanceof \WP_Term) {
+                $this->renderTermSocialMeta($term);
+                $this->renderMetaDescription($term->description);
+            } else {
+                $this->renderSocialMetaFallback();
+                $this->renderMetaDescription();
             }
         }
     }
@@ -165,11 +186,13 @@ class StoreKeeperSeoHandler implements WithHooksInterface
     protected function renderHeadMetaSeo(array $seo): void
     {
         $kw = esc_attr($seo[StoreKeeperSeo::SEO_KEYWORDS]);
-        $description = esc_attr($seo[StoreKeeperSeo::SEO_DESCRIPTION]);
-        echo <<<HTML
-  <meta name="description" content="$description">
+        if (!empty($kw)) {
+            echo <<<HTML
   <meta name="keywords" content="$kw">
 HTML;
+        }
+        $description = esc_attr($seo[StoreKeeperSeo::SEO_DESCRIPTION]);
+        $this->renderMetaDescription($description);
     }
 
     protected function renderProductSocialMeta(array $seo, \WC_Product $product): void
@@ -197,26 +220,133 @@ HTML;
         if (!empty($image_src)) {
             $image_src = $image_src[0];
         }
-        $image = esc_attr($image_src);
+        $image_src = esc_attr($image_src);
         $url = esc_attr(wp_get_canonical_url());
         $price = $product->get_sale_price('edit');
+        if (empty($price)) {
+            $price = $product->get_regular_price('edit');
+        }
         if ($product instanceof \WC_Product_Variable) {
             $price = $product->get_variation_price('min', false);
         }
         $currency = get_woocommerce_currency();
+        $this->renderSocialMeta('product', $title, $url, $description, $image_src);
         echo <<<HTML
-  <meta property="og:type" content="product">
-  <meta property="og:title" content="$title">
-  <meta property="og:url" content="$url">
-  <meta property="og:description" content="$description">
-  <meta property="og:image" content="$image">
   <meta property="product:price.amount" content="$price">
   <meta property="product:price.currency" content="$currency">
-  <meta name="twitter:card" content="summary_large_image">
-  <meta property="twitter:url" content="$url">
+HTML;
+    }
+
+    protected function renderPostSocialMeta(\WP_Post $post): void
+    {
+        $title = get_the_title($post);
+        $title = esc_attr($title);
+
+        $description = $post->post_excerpt;
+        $description = esc_attr(strip_tags(do_shortcode($description)));
+
+        $image_src = wp_get_attachment_image_src(
+            get_post_thumbnail_id($post->ID), 'single-post-thumbnail'
+        );
+        if (!empty($image_src)) {
+            $image_src = $image_src[0];
+        }
+        $image_src = esc_attr($image_src);
+        $url = esc_attr(wp_get_canonical_url());
+
+        $isPost = 'post' === $post->post_type;
+        $type = $isPost ? 'article' : 'website';
+
+        $this->renderSocialMeta($type, $title, $url, $description, $image_src);
+
+        if ($isPost) {
+            $mod = esc_attr(get_the_modified_date('c'));
+            $pub = esc_attr(get_the_date('c'));
+            $categories = get_the_category();
+            if (is_array($categories)) {
+                foreach ($categories as $category) {
+                    /* @var $category \WP_Term */
+                    $term_name = esc_attr($category->name);
+                    echo <<<HTML
+  <meta property="article:section" content="$term_name">
+HTML;
+                    break; // we show only first
+                }
+            }
+
+            echo <<<HTML
+  <meta property="article:published_time" content="$pub">
+  <meta property="article:modified_time" content="$mod">
+HTML;
+            $tags = get_the_tags();
+            if (is_array($tags)) {
+                foreach ($tags as $tag) {
+                    /* @var $tag \WP_Term */
+                    $term_name = esc_attr($tag->name);
+                    echo <<<HTML
+  <meta property="article:tag" content="$term_name">
+HTML;
+                }
+            }
+        }
+    }
+
+    protected function renderTermSocialMeta(\WP_Term $term): void
+    {
+        $title = esc_attr(wp_get_document_title());
+
+        $description = $term->description;
+        $description = esc_attr(strip_tags(do_shortcode($description)));
+
+        $this->renderSocialMeta('website', $title, null, $description);
+    }
+
+    protected function renderSocialMetaFallback(): void
+    {
+        $title = esc_attr(wp_get_document_title());
+
+        $this->renderSocialMeta('website', $title);
+    }
+
+    protected function renderSocialMeta(string $type, string $title, ?string $url = null, ?string $description = null, ?string $image_src = null): void
+    {
+        echo <<<HTML
+  <meta property="og:type" content="$type">
+  <meta property="og:title" content="$title">
   <meta name="twitter:title" content="$title">
+HTML;
+        if (!empty($url)) {
+            echo <<<HTML
+  <meta property="og:url" content="$url">
+  <meta property="twitter:url" content="$url">
+HTML;
+        }
+
+        if (empty($description)) {
+            $description = get_bloginfo('description');
+        }
+        if (!empty($description)) {
+            echo <<<HTML
+  <meta property="og:description" content="$description">
   <meta name="twitter:description" content="$description">
+HTML;
+        }
+        if (!empty($image_src)) {
+            echo <<<HTML
+  <meta name="twitter:card" content="summary_large_image">
+  <meta property="og:image" content="$image_src">
   <meta name="twitter:image" content="$image_src">
+HTML;
+        }
+    }
+
+    protected function renderMetaDescription(?string $description = null): void
+    {
+        if (empty($description)) {
+            $description = get_bloginfo('description');
+        }
+        echo <<<HTML
+<meta name="description" content="$description">
 HTML;
     }
 }
