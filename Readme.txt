84003eeafc10340ee7189d02501010f08a196f23
luki <szymon@storekeeper.nl>
2023-08-28 14:59:59 +0200

    Added testing for all version up to 8.2

--- Readme.txt
+++ Readme.txt
@@ -1,8 +1,8 @@
 === StoreKeeper for Woocommerce ===
 Tags: woocommerce,e-commerce, woo,sales,store
 Requires at least: 5.0
-Tested up to: 6.0.0
-Requires PHP: 7.3
+Tested up to: 6.3.0
+Requires PHP: 7.4
 Stable Tag: 0.0.1
 License: GNU General Public License v2.0
 License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
--- docker/docker-test-entrypoint.sh
+++ docker/docker-test-entrypoint.sh
@@ -8,9 +8,12 @@ env | grep VERSION
 echo -n "Installed Wordpress version: "
 wp core version --path=$WORPRESS_ROOT || exit 2
 
-echo "Making a copy of wordpress in the ./mount easier debugging."
-rsync -rclD --delete --exclude storekeeper-for-woocommerce $WORPRESS_DEV_DIR mount/ || exit 3
-
+if [ "$COPY_TO_MOUNT" == "1" ]; then
+  echo "Making a copy of wordpress in the ./mount easier debugging."
+  rsync -rclD --delete --exclude storekeeper-for-woocommerce $WORPRESS_DEV_DIR mount/ || exit 3
+else
+  echo "COPY_TO_MOUNT is not set to 1. No copy"
+fi
 cd $STOREKEEPER_PLUGIN_DIR/tests
 exec $WORPRESS_DEV_DIR/vendor/bin/phpunit .
 
