b46ace181608548b1344ecd6dc604e64be044777
luki <szymon@storekeeper.nl>
2024-08-26 15:52:41 +0200

    Added better field creation for the selection fields

--- src/StoreKeeper/WooCommerce/B2C/Frontend/Handlers/ProductAddOnHandler.php
+++ src/StoreKeeper/WooCommerce/B2C/Frontend/Handlers/ProductAddOnHandler.php
@@ -6,6 +6,11 @@ use StoreKeeper\WooCommerce\B2C\Hooks\WithHooksInterface;
 
 class ProductAddOnHandler implements WithHooksInterface
 {
+    public const ADDON_TYPE_SINGLE_CHOICE = 'single-choice';
+    public const ADDON_TYPE_MULTIPLE_CHOICE = 'multiple-choice';
+    public const ADDON_TYPE_REQUIRED_ADDON = 'required-addon';
+    public const ADDON_TYPE_BUNDLE = 'bundle'; // todo
+    const FIELD_PREFIX = 'sk_add_on_choice';
 
     public function registerHooks(): void
     {
@@ -16,6 +21,83 @@ class ProductAddOnHandler implements WithHooksInterface
 
     }
 
+    const PRODUCT_ADDONS = [
+        [
+            'addon_group_id' => 1,
+            'title' => 'Standaard inbegrepen',
+            'type' => self::ADDON_TYPE_REQUIRED_ADDON,
+            'options' => [
+                [
+                    'id' => 1,
+                    'title' => 'Zanddeeg 500 gram',
+                    'ppu_wt' => 2.85,
+                    'shop_product_id' => 123,
+                ],
+                [
+                    'id' => 2,
+                    'title' => 'Banketbakkersroom (bakvast) 100 gram (CaH)',
+                    'ppu_wt' => 1.85,
+                    'shop_product_id' => 123,
+                ],
+                [
+                    'id' => 3,
+                    'title' => 'Amandelspijs 100 gram',
+                    'ppu_wt' => 2.35,
+                    'shop_product_id' => 122,
+                ]
+            ]
+        ],
+        [
+            'addon_group_id' => 2,
+            'title' => 'Smaak bavarois',
+            'type' => self::ADDON_TYPE_SINGLE_CHOICE,
+            'options' => [
+                [
+                    'id' => 4,
+                    'title' => 'Bavarois Advocaat 100 gram',
+                    'ppu_wt' => 3.75,
+                    'shop_product_id' => 123,
+                ],
+                [
+                    'id' => 5,
+                    'title' => 'Bavarois Crème Brûlée',
+                    'ppu_wt' => 3.75,
+                    'shop_product_id' => 123,
+                ],
+                [
+                    'id' => 6,
+                    'title' => 'Bavarois Cappuccino 100 gram',
+                    'ppu_wt' => 3.75,
+                    'shop_product_id' => 123,
+                ],
+            ]
+        ],
+        [
+            'addon_group_id' => 4,
+            'title' => 'Smaak bavarois (extra)',
+            'type' => self::ADDON_TYPE_MULTIPLE_CHOICE,
+            'options' => [
+                [
+                    'id' => 7,
+                    'title' => 'Bavarois Banaan 100 gram',
+                    'ppu_wt' => 3.75,
+                    'shop_product_id' => 123,
+                ],
+                [
+                    'id' => 8,
+                    'title' => 'Bavarois Rabarber-Aardbei 100 gram',
+                    'ppu_wt' => 3.75,
+                    'shop_product_id' => 123,
+                ],
+                [
+                    'id' => 9,
+                    'title' => 'Bavarois Yoghurt-Kers 100 gram',
+                    'ppu_wt' => 3.75,
+                    'shop_product_id' => 123,
+                ],
+            ]
+        ]
+    ];
 
     public function add_addon_fields(): void
     {
@@ -25,32 +107,40 @@ class ProductAddOnHandler implements WithHooksInterface
             return;
         }
 
-        // todo check if add-on product
+        foreach (self::PRODUCT_ADDONS as $addon) {
+            $id = $addon['addon_group_id'];
+            if( $addon['type'] === self::ADDON_TYPE_REQUIRED_ADDON ) {
+                echo '<p class="custom-checkbox-description">'.$addon['title'].'</p>'; // todo style better
+                echo '<ul>';
+                foreach ($addon['options'] as $option) {
+                    echo '<li>'.$option['title'].'</li>';
+                }
+                echo '</ul>';
+            } else if( $addon['type'] === self::ADDON_TYPE_SINGLE_CHOICE ) {
+                $field_options = [];
+                foreach ($addon['options'] as $option) {
+                    $field_options[$option['id']] =  $option['title'] . ' (+' . $option['ppu_wt'] . ')';
+                }
+                woocommerce_form_field($this->getSingleKeyName($id), array(
+                    'type' => 'select',
+                    'class' => array('form-row-wide'),
+                    'label' => $addon['title'],
+                    'required' => true,
+                    'options' => $field_options
+                ));
+            } else if( $addon['type'] === self::ADDON_TYPE_MULTIPLE_CHOICE ) {
+                echo '<p class="custom-checkbox-description">'.$addon['title'].'</p>';
+                foreach ($addon['options'] as $option) {
+                    woocommerce_form_field($this->getMultipleChoiceKeyName($id, $option['id']), array(
+                        'type' => 'checkbox',
+                        'class' => array('form-row-wide'),
+                        'label' => $option['title'] . ' (+' . $option['ppu_wt'] . ')',
+                    ));
+                }
 
-        // todo single multiselects
-        woocommerce_form_field('custom_option', array(
-            'type' => 'select',
-            'class' => array('form-row-wide'),
-            'label' => __('Custom Option'),
-            'required' => true,
-            'options' => array(
-                '' => 'Please select',
-                'option1' => 'Product 1 (+1.00)',
-                'option2' => 'Product 3 (+1.00)'
-            )
-        ));
-        // todo render multiselects
-        echo '<p class="custom-checkbox-description">Extra addons for the cake:</p>';
-        woocommerce_form_field('custom_checkbox_1', array(
-            'type' => 'checkbox',
-            'class' => array('form-row-wide'),
-            'label' => __('Product 1 (+1.00)'),
-        ));
-        woocommerce_form_field('custom_checkbox_2', array(
-            'type' => 'checkbox',
-            'class' => array('form-row-wide'),
-            'label' => __('Product 2 (+1.00)'),
-        ));
+            }
+        }
+
     }
     function validate_custom_field($passed, $product_id, $quantity) {
         if (empty($_POST['custom_option'])) {
@@ -79,5 +169,15 @@ class ProductAddOnHandler implements WithHooksInterface
             );
         }
         return $item_data;
+    }
+
+    protected function getSingleKeyName($id): string
+    {
+        return self::FIELD_PREFIX ."[$id][".self::ADDON_TYPE_SINGLE_CHOICE."]";
+    }
+
+    protected function getMultipleChoiceKeyName($addon_id, $option_id): string
+    {
+        return self::FIELD_PREFIX ."[$addon_id][".self::ADDON_TYPE_MULTIPLE_CHOICE."][$option_id]";
     }
 }
