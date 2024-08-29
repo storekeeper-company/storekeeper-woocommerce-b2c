a6997f6995ad186cb1e8dd802d39ee0b9bd7fddc
luki <szymon@storekeeper.nl>
2024-08-29 17:56:46 +0200

    added missing escape

--- src/StoreKeeper/WooCommerce/B2C/templates/add-on/form-required.php
+++ src/StoreKeeper/WooCommerce/B2C/templates/add-on/form-required.php
@@ -5,7 +5,7 @@ echo '<div class="sk-addon-select sk-addon-'.$type.'">';
 echo '<p class="sk-addon-title">'.esc_html($addon['title']).'</p>';
 echo '<ul>';
 foreach ($addon['options'] as $option) {
-    echo '<li>'.$option['title'].'</li>';
+    echo '<li>'.esc_html($option['title']).'</li>';
 }
 echo '</ul>';
 echo '</div>';
