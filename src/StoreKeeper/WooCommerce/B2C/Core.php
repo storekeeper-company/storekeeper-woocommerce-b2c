bd77e159da1c62ba3f263c35962ef0ce64e51921
danielastresna <dstresna@backstage-it.md>
2024-10-15 16:30:17 +0300

    added I18N::DOMAIN for translation

--- src/StoreKeeper/WooCommerce/B2C/Core.php
+++ src/StoreKeeper/WooCommerce/B2C/Core.php
@@ -146,7 +146,7 @@ class Core
     public function __construct()
     {
         add_action('init', function() {
-            load_plugin_textdomain('free-shipping', false, 'storekeeper-for-woocommerce/languages/');
+            load_plugin_textdomain(I18N::DOMAIN, false, 'storekeeper-for-woocommerce/languages/');
         });
 
         // Declare HPOS compabitility
